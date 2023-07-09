7e91730f155d65e5a2999942516f7760122a6926
Ivan Janjić <wannjanjic@gmail.com>
2023-07-09 15:39:13 +0200

    add my get endpoint

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -6,11 +6,17 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/name")]
+async fn tell_name() -> &'static str {
+    "My name is Wann!"
+}
+
 #[shuttle_runtime::main]
 async fn actix_web(
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
+        cfg.service(tell_name);
     };
 
     Ok(config.into())
