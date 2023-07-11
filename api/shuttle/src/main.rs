e80745517c7af3a64e3614dfe18dbd47683ffc53
Ivan Janjić <wannjanjic@gmail.com>
2023-07-11 19:00:15 +0200

    refactor film endpoints to use generics

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -15,14 +15,16 @@ async fn actix_web(
     // let pool = actix_web::web::Data::new(pool);
     let film_repository = api_lib::film_repository::PostgresFilmRepository::new(pool);
     // let film_repository = actix_web::web::Data::new(film_repository);
-    let film_repository: actix_web::web::Data<Box<dyn api_lib::film_repository::FilmRepository>> =
-        actix_web::web::Data::new(Box::new(film_repository));
+    // let film_repository: actix_web::web::Data<Box<dyn api_lib::film_repository::FilmRepository>> =
+    //     actix_web::web::Data::new(Box::new(film_repository));
+    let film_repository = actix_web::web::Data::new(film_repository); // use generics instead of the dynamics
 
     let config = move |cfg: &mut ServiceConfig| { 
         // cfg.app_data(pool)
         cfg.app_data(film_repository)
             .configure(api_lib::health::service)
-            .configure(api_lib::films::service);          
+            // .configure(api_lib::films::service);     
+            .configure(api_lib::films::service::<api_lib::film_repository::PostgresFilmRepository>); // use generics insted of the dynamics     
     };
 
     Ok(config.into())
