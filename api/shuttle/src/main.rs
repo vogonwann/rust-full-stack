d3c60b1f9da5ba2b0deab2724d5ac45711cc2f80
Ivan Janjić <wannjanjic@gmail.com>
2023-07-11 15:12:30 +0200

    feat: add films endpoints

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -14,8 +14,10 @@ async fn actix_web(
     
     let pool = actix_web::web::Data::new(pool);
 
-    let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(pool).configure(api_lib::health::service);            
+    let config = move |cfg: &mut ServiceConfig| { 
+        cfg.app_data(pool)
+            .configure(api_lib::health::service)
+            .configure(api_lib::films::service);          
     };
 
     Ok(config.into())
